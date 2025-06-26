d18c20888d7fa47b80a01ef0f7a87db37e0de4ff
Itsiami <mariontallandier@gmail.com>
2025-06-26 11:53:30 +0200

    Create main
    Change type of project
    Create main
    Read the file

---
+++ exercise/week01/c#/Submarine/Submarine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Submarine
+{
+    public static void Main(string[] args)
+    {
+        string filePath = @"C:\\Users\\itsia\\Documents\\GitHub\\Repo\\cahier-de-vacances-b3\\exercise\\week01\\c#\\Submarine.Tests\\submarine.txt";
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
